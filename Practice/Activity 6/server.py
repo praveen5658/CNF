5a1ff2f766582d99a29b751829a18968f6d95634
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-12-19 18:11:22 +0530

    Activity 6

---
+++ Practice/Activity 6/server.py
@@ -0,0 +1,19 @@
+import socket
+
+def main():
+	host = '127.0.0.1'
+	port = 5000
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind((host,port))
+	print("Server Started")
+	while True:
+		data , addr = s.recvfrom(1024)
+		data = data.decode()
+		print("message from: " + str(addr))
+		print("from connected user: " + str(data))
+		data = str(data).upper()
+		print("Sending: " + str(data))
+		s.sendto(data.encode(),addr)
+	s.close()
+if __name__ == '__main__':
+	main()
